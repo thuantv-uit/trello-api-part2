9dc367b69acdcb28cf76bce471bf6fe70f7a7aef
tvt04102004@gmail.com <tvt04102004@gmail.com>
2025-01-27 11:07:27 +0700

    Init project

---
+++ src/utils/constants.js
@@ -0,0 +1,10 @@
+// Những domain được phép truy cập đến domain của server
+export const WHITELIST_DOMAINS = [
+  'http://localhost:5173'
+// Về sau này sẽ deploy lên domain chính thức
+]
+
+export const BOARD_TYPES = {
+  PUBLIC: 'public',
+  PRIVATE: 'private'
+}
